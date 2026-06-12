de9aaf9bf577e082c47eaba9ebaa492b360e3384
Juan José García Duarte <gdjuanjojr@gmail.com>
2026-06-13 00:50:40 +0200

    [ENV] Improve launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,14 @@
 #bin/sh
-konsole -e bash  -c  "cd src_de_base/ && make debug; exec bash" & 
-gdb -ex 'target remote:1234' -ex  'b kernel_start'  src_de_base/kernel.bin 
+#If any command fails, we stop script execution
+set -e
 
+PROJECT_FOLDER=src_de_base/
+
+cd $PROJECT_FOLDER
+# Compile the project latest version
+make
+
+
+konsole -e bash  -c  "make debug; exec bash" & 
+gdb -ex 'target remote:1234' -ex  'b kernel_start'  kernel.bin 
+ps axf | grep "make debug" | grep -v grep | awk '{print "kill -9 " $1}' | sh
