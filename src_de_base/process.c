40098ee7ae83ca4ba2e28cd8cd8f0d58531651da
Juan José García Duarte <gdjuanjojr@gmail.com>
2026-07-13 08:40:30 +0200

    feat(process): Initialize process function + define process on header.

--- src_de_base/process.c
+++ src_de_base/process.c
@@ -1,32 +1,28 @@
 #include "inttypes.h"
+#include <debug.h>
+#include <string.h>
+#include "process.h"
 
-#define PROCESS_STACK_SIZE 512
-enum process_state {
-    CHOSEN,
-    SLEEPING,
-    ACTIVABLE,
-};
+#define NUMBER_OF_REGISTERS 5
+int32_t pid = 0;
+struct Process os_processes[5];
 
-struct Process {
-    int pid;
-    char name[20];
-    enum process_state state;
-    uint32_t register_table[5];
-    int call_stack[PROCESS_STACK_SIZE];
-};
 
 // TODO: Right now is created locally, after going out of the function, the stack of this function will collapse and
 // the struct memory wont be valid for using.
 //
 // I should use malloc here, or pass by pointer.
-struct Process new_process() {
-    struct Process p1 = {
-        .pid = 1,
-        .name = "process_dummy",
-        .state = CHOSEN,
-        .register_table = {0,0,0,0,0},
-        .call_stack = {0},
-    };
+void initialize_process(struct Process  *p2, char * name,  void (*process_fn)()) {
+    assert(name != 0);
+    pid++;
+    strcpy(p2->name, name);
+    p2->pid = pid;
+    p2->state = READY;
+    memset(p2->register_table, 0, NUMBER_OF_REGISTERS * 4);
+    memset(p2->call_stack, 0, PROCESS_STACK_SIZE * 4);
+    p2->call_stack[PROCESS_STACK_SIZE-1] = (uint32_t)process_fn;
+    p2->register_table[1] = (uint32_t) &p2->call_stack[PROCESS_STACK_SIZE-1];
+}
 
     return p1;
 }
