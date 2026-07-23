abaf6945d826982c73566497f9a895aaa8135934
Juan José García Duarte <gdjuanjojr@gmail.com>
2026-07-23 21:47:11 +0200

    refacto: Declare global variables with a `g_` prefix

--- src_de_base/scheduler.c
+++ src_de_base/scheduler.c
@@ -5,16 +5,14 @@
 #include "inttypes.h"
 #include "time.h"
 #include "stdio.h"
-#include "cpu.h"
 #define MAX_NUM_OF_PROCESSES 5
 
 extern void ctx_sw(int32_t * old_context, int32_t * new_context);
 
-struct Process os_processes[MAX_NUM_OF_PROCESSES];
-int32_t number_of_processes_created = 0;
-int32_t free_place = 0;
+static struct Process g_process_control_block_table[MAX_NUM_OF_PROCESSES];
+static struct Process * g_active_process = &g_process_control_block_table[0];
+static int32_t g_number_of_processes_spawned = 0;
 
-struct Process * active_process = &os_processes[0];
 static void idle_process_initialization(struct Process * p);
 static void wake_up_sleeping_processes();
 static void display_processes_state();
@@ -23,7 +21,7 @@ static void end_process();
 void schedule() {
     int32_t chosen_process_pid = 0;
     struct Process * chosen_process;
-    struct Process * current_process = active_process;
+    struct Process * current_process = g_active_process;
 
     wake_up_sleeping_processes();
 
@@ -32,7 +30,7 @@ void schedule() {
     // We could begin to search from the pid immediatly after the active process id
     // and finish searching on the pid before our active process id.
     while(chosen_process_pid < MAX_NUM_OF_PROCESSES) {
-        if (os_processes[chosen_process_pid].state == READY)
+        if (g_process_control_block_table[chosen_process_pid].state == READY)
             break;
         chosen_process_pid++;
     }
@@ -41,13 +39,13 @@ void schedule() {
         return;
     }
     assert(chosen_process_pid < MAX_NUM_OF_PROCESSES);
-    chosen_process = &os_processes[chosen_process_pid];
+    chosen_process = &g_process_control_block_table[chosen_process_pid];
 
     // Update processes states
-    if (active_process->state != SLEEPING && active_process->state != ZOMBIE)
-        active_process->state = READY;  // active process
+    if (g_active_process->state != SLEEPING && g_active_process->state != ZOMBIE)
+        g_active_process->state = READY;  // active process
     chosen_process->state = CHOSEN; // next active process
-    active_process = &os_processes[chosen_process_pid]; // update active_process pointer
+    g_active_process = &g_process_control_block_table[chosen_process_pid]; // update active_process pointer
     display_processes_state();
     ctx_sw((int32_t *)current_process->register_table, (int32_t *)chosen_process->register_table);
 }
@@ -63,7 +61,8 @@ void schedule() {
 int32_t new_process(char * name,  void (*process_fn)()) {
     // TODO: choose free slot policy is always pointing to the first cases first
     // Is it a desirable behavior?
-    number_of_processes_created++;
+    g_number_of_processes_spawned++;
+    uint32_t free_place = 0;
 
     // Verify that the esp is not accessing addresses outside its dedicated stack
     // Verify at the beginning and at the end.
@@ -71,16 +70,16 @@ int32_t new_process(char * name,  void (*process_fn)()) {
     assert(name != 0);
     assert(process_fn != 0);
     for (free_place = 1; free_place < MAX_NUM_OF_PROCESSES; free_place++){
-        enum process_state process_state = os_processes[free_place].state;
+        enum process_state process_state = g_process_control_block_table[free_place].state;
         if (process_state == UNINITIALIZED || process_state == ZOMBIE)
             break;
     }
     if (free_place  >= MAX_NUM_OF_PROCESSES)
         return -1;
 
-    struct Process *process = &os_processes[free_place];
-    process->pid = free_place;
-    sprintf(process->name, "PROC %d", free_place);
+    struct Process *process = &g_process_control_block_table[free_place];
+    process->pid = g_number_of_processes_spawned;
+    sprintf(process->name, "PROC %d", g_number_of_processes_spawned);
     process->state = READY;
     memset(process->register_table, 0, NUMBER_OF_REGISTERS * 4); // 4 bytes each register
     memset(process->call_stack, 0, PROCESS_STACK_SIZE * 4); // for bytes each stack case
@@ -94,12 +93,12 @@ int32_t new_process(char * name,  void (*process_fn)()) {
 }
 
 void end_process() {
-    active_process->state = ZOMBIE;
+    g_active_process->state = ZOMBIE;
     schedule();
 }
 
 void setup_scheduler() {
-    idle_process_initialization(&os_processes[0]);
+    idle_process_initialization(&g_process_control_block_table[0]);
 }
 
 /*
@@ -112,18 +111,18 @@ static void idle_process_initialization(struct Process *p) {
 }
 
 void sleep(uint32_t number_of_seconds) {
-    active_process->waking_time = uptime() + number_of_seconds;
-    active_process->state = SLEEPING;
+    g_active_process->waking_time = uptime() + number_of_seconds;
+    g_active_process->state = SLEEPING;
     schedule();
 }
 
 void wake_up_sleeping_processes(){
     for(int pid = 1; pid < MAX_NUM_OF_PROCESSES; pid ++) {
-        if (os_processes[pid].state != SLEEPING)
+        if (g_process_control_block_table[pid].state != SLEEPING)
             continue;
 
-        if (os_processes[pid].waking_time <= uptime()) {
-            os_processes[pid].state = READY;
+        if (g_process_control_block_table[pid].waking_time <= uptime()) {
+            g_process_control_block_table[pid].state = READY;
         }
     }
 }
@@ -135,8 +134,8 @@ void display_processes_state(){
     uint32_t column = cursor_column();
     update_cursor_on_screen(0, 0);
     for (int i = 0; i < MAX_NUM_OF_PROCESSES; i++) {
-        name = os_processes[i].name;
-        state = process_state_name[os_processes[i].state];
+        name = g_process_control_block_table[i].name;
+        state = process_state_name[g_process_control_block_table[i].state];
         printf("[%s\t] pid = %i\tstate: %s\t\t\n", name, i, state);
     }
     update_cursor_on_screen(line, column);
@@ -146,12 +145,12 @@ void display_processes_state(){
  * Returns the active process name
  */
 char *name() {
-    return active_process->name;
+    return g_active_process->name;
 }
 
 /*
  * Returns the active process pid
  */
 int32_t pid() {
-    return active_process->pid;
+    return g_active_process->pid;
 }
